e720e48a03dc349152e34fb38ecf22446fbddc14
PonizhenDoRyadovogo <art25ivan@gmail.com>
2026-04-19 16:35:36 +0700

    Изменения
    1. Определил структуру для хранения результата чтения BMP файла
    2. Определил структуры заголовков BMP файла

---
+++ yuv_overlay_test/bmp_reader.cpp
@@ -0,0 +1,31 @@
+﻿#include <fstream>
+#include <stdexcept>
+
+#include "bmp_reader.h"
+
+#pragma pack(push, 1)
+struct BmpFileHeader {
+	uint16_t bfType;
+	uint32_t bfSize;
+	uint16_t bfReserved1;
+	uint16_t bfReserved2;
+	uint32_t bfOffBits;
+};
+struct BmpInfoHeader {
+	uint32_t biSize;
+	uint32_t biWidth;
+	uint32_t biHeight;
+	uint16_t biPlanes;
+	uint16_t biBitCount;
+	uint32_t biCompression;
+	uint32_t biSizeImage;
+	int32_t biXPelsPerMeter;
+	int32_t biYPelsPerMeter;
+	uint32_t biClrsUsed;
+	uint32_t biClrImportant;
+};
+#pragma pack(pop)
+
+RgbImage readBmp24(const std::string& path) {
+
+}
--- yuv_overlay_test/bmp_reader.h
+++ yuv_overlay_test/bmp_reader.h
@@ -1,6 +1,18 @@
 ﻿#ifndef BMP_READER
 #define BMP_READER
 
+#include <cstdint>
+#include <string>
+#include <vector>
 
+struct RgbImage {
+	int width;
+	int height;
+	std::vector<uint8_t> data;
+
+	RgbImage() : width(0), height(0) {}
+};
+
+RgbImage 
 
 #endif // BMP_READER
